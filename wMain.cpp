2dca4c84c0a0a477800f7dc85997e330ab26fd9d
wanvia <lairu13kps18@outlook.jp>
2024-05-05 09:57:07 +0900

    aaa

--- wMain.cpp
+++ wMain.cpp
@@ -2,22 +2,24 @@
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	HWND hWnd;
 	WNDCLASSEX wc;
+	MSG msg;
+	// TCHAR szWindowClass[] = TEXT("Sample02");
 
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
 	wc.lpfnWndProc = WindowProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
+	wc.hInstance = hInst;
 	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wc.lpszMenuName = NULL;
-	wc.lpszClassName = TEXT("firstWindow");
+	wc.lpszClassName = TEXT("Sample02");
 	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 	RegisterClassEx(&wc);
 
@@ -27,19 +29,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		return 0;
 	}
 
-	hWnd = CreateWindowEx(WS_EX_ACCEPTFILES, TEXT("firstWindow"), TEXT("lol"), WS_OVERLAPPEDWINDOW, 0, 0, 530, 230, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindowEx(WS_EX_ACCEPTFILES, TEXT("Sample02"), TEXT("lol"), WS_OVERLAPPEDWINDOW, 0, 0, 530, 230, NULL, NULL, hInst, NULL);
 
 	if (!hWnd)
 	{
-		MessageBox(NULL, TEXT("クリエイト"), TEXT(""), MB_OK);
+		MessageBox(NULL, TEXT("cre"), TEXT(""), MB_OK);
 		return 0;
 	}
 
-	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(hWnd, SW_SHOW);
 	UpdateWindow(hWnd);
 
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0) == 1)
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
